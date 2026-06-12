a8723065954a97b52e48d9485dfc9770b861885b
Taedddsdfs <115664141+Taedddsdfs@users.noreply.github.com>
2026-06-12 21:31:17 +0100

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = cursum = 0
+        prefixSums = {0:1} # prefix sum 이 0인 상태가 1번 존재한다
+        for num in nums:
+            cursum+=num
+            diff = cursum-k
+            res+=prefixSums.get(diff,0)
+            prefixSums[cursum] =  1+prefixSums.get(cursum,0)
+
+        return res
