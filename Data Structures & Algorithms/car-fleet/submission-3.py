4db074cd1e405bf6278e7a4f35f342f98d625603
Taedddsdfs <115664141+Taedddsdfs@users.noreply.github.com>
2026-06-20 19:23:25 +0100

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleet = [(p,s) for p,s in zip(position,speed)]
+        stack = []
+        fleet.sort(reverse = True) #target 에 가까운 차부터 딴차가 따라잡는지
+        for p,s in fleet:        
+            stack.append((target-p)/s) # time
+            while len(stack)>=2 and stack[-1]<=stack[-2]: # 뒤차시간<=앞차시간
+                stack.pop()
+
+        return len(stack)
