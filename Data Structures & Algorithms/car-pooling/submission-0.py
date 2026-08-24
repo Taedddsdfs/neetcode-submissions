78108e52039820136b77c1219d86849792624522
Taedddsdfs <115664141+Taedddsdfs@users.noreply.github.com>
2026-08-24 02:15:27 +0100

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        change = [0]*1001
+
+        for num,start,end in trips:
+            change[start]+=num
+            change[end]-=num
+        
+        passengers = 0
+        for x in change:
+            passengers+=x
+
+            if passengers>capacity:
+                return False
+        return True
